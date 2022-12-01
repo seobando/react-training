83908d32f31a5c819dfedbbfc6dc9c5458bd90d2
Sebastian OM <sebastianobandomorales@gmail.com>
2022-11-30 20:45:31 -0500

    using the previous state

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,25 +3,27 @@ import { useState } from 'react'
 
 
 function App() {
-  const [name, setName] = useState('mario')
   const [events, setEvents] = useState([
     {title: "mario's birthday bash",id:1},
     {title: "bowser's live stream",id:2},
     {title: "race on moo moo farm",id:3},
   ])
 
-  const handleClick = () => {
-    setName('luigi')
-    console.log(name)
+  const handleClick = (id) => {
+    setEvents((prevEvents) => { 
+      return prevEvents.filter((event) => {
+        return id !== event.id
+    })
+  })
+    console.log(id)
   }
 
   return (
     <div className='App'>
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-      {events.map((event) => (
+      {events.map((event,index) => (
         <div key={event.id}>
-          <h2>{event.title}</h2>
+          <h2>{index} - {event.title}</h2>
+          <button onClick = {() => handleClick(event.id)}>delete event</button>
         </div>
       ))}
     </div>
