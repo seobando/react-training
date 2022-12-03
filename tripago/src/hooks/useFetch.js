6eb0af7a65032edfe4c4a9cad4909f424686f0e9
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 18:44:42 -0500

    Aborting Fetch Requests

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -1,33 +1,42 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
-    const [data,setData] = useState(null)
-    const [isPending,setIsPending] = useState(false)
-    const [error, setError] = useState(null)
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            setIsPending(true)
+  useEffect(() => {
+    const controller = new AbortController();
 
-            try{
-                const res = await fetch(url)
-                if (!res.ok){
-                    throw new Error(res.statusText)
-                }
-                const json = await res.json()
-                
-                setIsPending(false)
-                setData(json)
-                setError(null)
-            } catch (err) {
-                setIsPending(false)
-                setError('Could not fetch the data')
-                console.log(err.message)
-            }   
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
         }
-        fetchData()
-    },[url])
+        const json = await res.json();
 
-    return {data, isPending, error}
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("the fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+          console.log(err.message);
+        }
+      }
+    };
+    fetchData();
 
-}
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
