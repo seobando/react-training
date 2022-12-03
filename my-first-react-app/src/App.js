58d9a8c292677c08d90a6bfcd23f2c39f2504dca
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 09:09:55 -0500

    The OnChange event

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Title from './components/Title'
 import Modal from './components/Modal';
 import EventList from './components/EventList';
+import NewEventForm from './components/NewEventForm';
 
 function App() {
   const [showModal,setShowModal] = useState(false)
@@ -51,12 +52,11 @@ function App() {
       {showEvents && <EventList events={events} handleClick={handleClick}/>}
 
       {showModal && <Modal handleClose={handleClose} isSalesModal={true}>
-        <h2>10% off Coupon Code!!</h2>
-        <p>Use the code NINJA10 at the checkout.</p>
+        <NewEventForm />
       </Modal>}
 
       <div>
-        <button onClick={() => setShowModal(true)}>Show Modal</button>
+        <button onClick={() => setShowModal(true)}>Add New Event</button>
       </div>    
 
     </div>
---
+++ my-first-react-app/src/components/NewEventForm.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './NewEventForm.css'
+
+export default function NewEventForm(){
+
+    const [title, setTitle] = useState('')
+    const [date,setDate] = useState('')
+
+    return(
+        <div>
+            <form className='new-event-form'>
+                <label>
+                    <span>Event Title:</span>
+                    <input type="text" onChange={(e)=>setTitle(e.target.value)}/> 
+                </label>
+                <label>
+                    <span>Event Date:</span>
+                    <input type="date" onChange={(e)=>setDate(e.target.value)}/> 
+                </label>
+                <button>Submit</button>        
+                <p>title -{title}, date - {date}</p>       
+            </form>
+        </div>
+    )
+}
