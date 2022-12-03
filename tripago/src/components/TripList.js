1831a0eeb1f3071306c51f635c30112a73188699
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 18:04:23 -0500

    useCallback for Function Dependencies

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 
 //styless
 import './TripList.css'
@@ -8,11 +8,15 @@ export default function TripList(){
     const [trips,setTrips] = useState([])
     const [url,setUrl] = useState('http://localhost:3000/trips')
 
-    useEffect(() => {
-        fetch(url)
-        .then(response => response.json())
-        .then(json => setTrips(json))
+    const fetchTrips = useCallback(async () => {
+        const response = await fetch(url)
+        const json = await response.json()
+        setTrips(json)
     }, [url])
+
+    useEffect(() => {
+        fetchTrips()
+    }, [fetchTrips])
 
     return(
         <div className="trip-list">
