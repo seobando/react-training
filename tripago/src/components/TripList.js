3a7ee67d66c9fb9f76599c4e48dc8ea7aae7aba7
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 18:24:30 -0500

    Adding a Loading/Pending State

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -8,11 +8,12 @@ export default function TripList(){
 
     const [url,setUrl] = useState('http://localhost:3000/trips')
 
-    const { data:trips } = useFetch(url)
+    const { data:trips,isPending } = useFetch(url)
 
     return(
         <div className="trip-list">
             <h2>TripList</h2>
+            {isPending && <div>Loading trips...</div>}
             <ul>
                 {trips && trips.map(trip => (
                     <li key={trip.id}>
