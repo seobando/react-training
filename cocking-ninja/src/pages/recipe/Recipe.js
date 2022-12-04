cc80057bb7874928647949b119ffbe15c5ffa952
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 10:43:45 -0500

    Recipe Details Template

--- cocking-ninja/src/pages/recipe/Recipe.js
+++ cocking-ninja/src/pages/recipe/Recipe.js
@@ -6,14 +6,23 @@ import './Recipe.css'
 
 export default function Recipe() {
   const { id } = useParams()
-  const url = 'https://localhost:3000/recipes/' + id
+  const url = 'http://localhost:3000/recipes/' + id
   const { data:recipe , isPending, error } = useFetch(url)
   
   return (
     <div className='recipe'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>loading...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
