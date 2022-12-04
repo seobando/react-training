0e827f352bc8cf9076c96316d954065c7447237d
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 20:07:36 -0500

    Project Preview & Setup

---
+++ magic-memory/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <butto>New Game</butto>
+    </div>
+  );
+}
+
+export default App;
