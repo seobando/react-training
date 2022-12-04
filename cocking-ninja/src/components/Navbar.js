c23f6caa845e574ce3c2f6dda430935edb207d15
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 12:10:34 -0500

    CHALLENGE - Redirect the User

--- cocking-ninja/src/components/Navbar.js
+++ cocking-ninja/src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 // styles
 import './Navbar.css'
 
+// components
+import SearchBar from './SearchBar'
+
 export default function Navbar() {
   return (
     <div className='navbar'>
@@ -10,6 +13,7 @@ export default function Navbar() {
             <Link to="/" className='brand'>
                 <h1>Cooking Ninja</h1>
             </Link>
+            <SearchBar/>
             <Link to="/create">
                 <h1>Create Recipe</h1>
             </Link>                        
