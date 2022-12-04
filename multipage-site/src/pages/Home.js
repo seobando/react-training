30973e71693fc57a501007617df6163273eb6383
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 07:24:22 -0500

    Fetching Data

--- multipage-site/src/pages/Home.js
+++ multipage-site/src/pages/Home.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { useFetch } from '../hooks/useFetch'
+
+//styles
+import './Home.css'
 
 export default function Home() {
+
+  const {data:articles,isPending,error} = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum,
-        leo a consequat varius, risus est consectetur ligula, in porta augue
-        ipsum eu ante. Cras in erat ac magna mollis tempus. Vivamus sed diam
-        euismod, euismod sapien sit amet, suscipit sapien. Praesent facilisis
-        auctor nisi, vel faucibus ex euismod eget. Donec vitae fringilla mi. In
-        in nibh id ex pharetra vulputate. Phasellus auctor felis elit, nec
-        dictum lacus bibendum ut. Nulla quis sollicitudin purus. Vivamus posuere
-        dui at dolor lobortis, eget ultrices ligula sodales. In vitae enim sit
-        amet felis ullamcorper blandit. Maecenas purus quam, commodo in
-        dignissim sit amet, eleifend eu nulla. Suspendisse sit amet blandit
-        velit. In nec sodales diam, nec blandit justo. Vivamus rutrum metus et
-        est scelerisque, vitae scelerisque augue convallis. Nunc semper eu neque
-        commodo fringilla. Maecenas tincidunt erat vitae vulputate pharetra.
-      </p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles && articles.map((article) => (
+        <div key={article.id} className="card">
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+        </div>
+      ))}
+
     </div>
   );
 }
