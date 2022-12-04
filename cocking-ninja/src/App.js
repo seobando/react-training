d0375265d33bb8e4f40ba6216e9bd5fc4b63c8c0
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 08:58:59 -0500

    Router and Pages Setup

--- cocking-ninja/src/App.js
+++ cocking-ninja/src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRoute, Switch, Route } from 'react-router-dom'
 import './App.css'
+
+// page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BrowserRoute>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe/>
+          </Route>
+        </Switch>
+      </BrowserRoute>
     </div>
   );
 }
