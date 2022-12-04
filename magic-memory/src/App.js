2d0aa37eb0e7c3982c8afc6225f1065d666c3d58
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 20:49:35 -0500

    CHALLENGE - Comparing Choices

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
@@ -32,7 +32,26 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
-  //
+
+  // compare 2 selected cards
+  useEffect(()=>{
+    if (choiceOne && choiceTwo){
+      if (choiceOne.src === choiceTwo.src){
+        console.log('those cards do match')
+        resetTurn()
+      } else {
+        console.log('those cards do not match')
+        resetTurn()
+      }
+    }
+  },[choiceOne,choiceTwo])
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
     <div className="App">
