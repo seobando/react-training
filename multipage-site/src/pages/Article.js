249d9bf047806796dd49046be247b00980b4023b
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 07:49:21 -0500

    Programmtic Redirects

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,10 +1,22 @@
-import { useParams } from "react-router-dom"
-import { useFetch } from "../hooks/useFetch"
+import { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 export default function Article() {
-  const { id } = useParams()
-  const url = 'http://localhost:3000/articles/' + id
-  const { data:article, isPending, error } = useFetch(url)
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: article, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      //redirect
+      //history.goBack()
+      setTimeout(() => {
+        history.push("/");
+      }, 2000) 
+    }
+  }, [error,history]);
 
   return (
     <div>
@@ -12,11 +24,11 @@ export default function Article() {
       {error && <div>{error}</div>}
       {article && (
         <div>
-            <h2>{article.title}</h2>
-            <p>By {article.author}</p>
-            <p>{article.body}</p>
+          <h2>{article.title}</h2>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
         </div>
       )}
     </div>
-  )
+  );
 }
