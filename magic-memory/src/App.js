489c4fcdc2c82e413abaadbdbb9d82c14e39906d
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-03 20:19:36 -0500

    Setting up and Shuffling Cards

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [truns,setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0)
+  };
+
+  //
+
+  //
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <butto>New Game</butto>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
