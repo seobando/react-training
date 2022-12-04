cb2c44955863b63f66468118ca1dfc23ded917cc
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-04 07:52:01 -0500

    Redirect Component

--- multipage-site/src/App.js
+++ multipage-site/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from "react-router-dom";
 
 // page components
 import Home from "./pages/Home";
@@ -31,6 +31,9 @@ function App() {
           </Route>
           <Route path="/article/:id">
             <Article/>
+          </Route>
+          <Route path="*">
+            <Redirect to="/"/>
           </Route>
         </Switch>
 
