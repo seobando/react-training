1bdda124265ca5101ac26eb23b0efa8913ccf4bc
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-05 07:44:26 -0500

    Fetching a Firestore

--- cocking-ninja/src/pages/recipe/Recipe.js
+++ cocking-ninja/src/pages/recipe/Recipe.js
@@ -1,30 +1,53 @@
-import { useParams } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch'
-import { useTheme } from '../../hooks/useTheme'
+import { useParams } from "react-router-dom";
+import { useTheme } from "../../hooks/useTheme";
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../../firebase/config";
 
 // styles
-import './Recipe.css'
+import "./Recipe.css";
 
 export default function Recipe() {
-  const { id } = useParams()
-  const url = 'http://localhost:3000/recipes/' + id
-  const { data:recipe , isPending, error } = useFetch(url)
-  const { mode } = useTheme()
-  
+  const { id } = useParams();
+  const { mode } = useTheme();
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError(`Could not find that recipe`);
+        }
+      });
+  }, [id]);
+
   return (
     <div className={`recipe ${mode}`}>
-      {error && <p className='error'>{error}</p>}
-      {isPending && <p className='loading'>loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">loading...</p>}
       {recipe && (
         <>
-          <h2 className='page-title'>{recipe.title}</h2>
+          <h2 className="page-title">{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
           </ul>
-          <p className='method'>{recipe.method}</p>
+          <p className="method">{recipe.method}</p>
         </>
       )}
     </div>
-  )
+  );
 }
