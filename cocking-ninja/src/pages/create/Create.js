37a46e41edd9127e20a145eec0660bf23600355b
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-05 07:53:38 -0500

    Adding Firestore Data

--- cocking-ninja/src/pages/create/Create.js
+++ cocking-ninja/src/pages/create/Create.js
@@ -1,44 +1,43 @@
 // styles
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config'
 
 import "./Create.css";
 
 export default function Create() {
-  const [title, setTitle] = useState("");
-  const [method, setMethod] = useState("");
-  const [cookingTime, setCookingTime] = useState("");
-  const [newIngredient,setNewIngedient] = useState('')
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+  const [newIngredient,setNewIngredient] = useState('')
   const [ingredients,setIngredients] = useState([])
   const ingredientInput = useRef(null)
+
   const navigate = useNavigate()
 
-  const { postData, data } = useFetch("http://localhost:3000/recipes","POST")
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    postData({title,ingredients,method,cookingTime: cookingTime + ' minutes'})
+    const doc = { title, ingredients, method, cookingTime: cookingTime + ' minutes' }
+
+    try {
+      await projectFirestore.collection('recipes').add(doc)
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }   
   };
 
   const handleAdd = (e) => {
     e.preventDefault()
     const ing = newIngredient.trim()
 
-    if (ing && !ingredients.includes(ing)){
-      setIngredients(prevIngredients => [...prevIngredients,ing])
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    setNewIngedient('')
+    setNewIngredient('')
     ingredientInput.current.focus()
   }
 
-
-  // redirect the user when we get data response
-  useEffect(()=>{
-    if (data){
-      navigate("/")
-    }
-  },[data,navigate])
 
   return (
     <div className="create">
@@ -58,7 +57,7 @@ export default function Create() {
           <div className="ingredients">
           <input 
             type="text" 
-            onChange={(e)=>setNewIngedient(e.target.value)}
+            onChange={(e)=>setNewIngredient(e.target.value)}
             value={newIngredient}
             ref={ingredientInput}
           />
