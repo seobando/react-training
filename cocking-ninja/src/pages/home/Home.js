1801a9dd6357ca667e87d18476701cf5cc8d7e99
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-07 05:29:58 -0500

    Real-Time Collection Data

--- cocking-ninja/src/pages/home/Home.js
+++ cocking-ninja/src/pages/home/Home.js
@@ -14,11 +14,8 @@ export default function Home() {
 
   useEffect(() => {
     setIsPending(true);
-
-    projectFirestore
-      .collection("recipes")
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection("recipes").onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError("No recipes to load");
           setIsPending(false);
@@ -27,13 +24,15 @@ export default function Home() {
           snapshot.docs.forEach((doc) => {
             results.push({ id: doc.id, ...doc.data() });
           });
-          setData(results)
-          setIsPending(false)
+          setData(results);
+          setIsPending(false);
         }
-      }).catch(err => {
-        setError(err.message)
-        setIsPending(false)
+      },
+      (err) => {
+        setError(err.message);
+        setIsPending(false);
       });
+      return () => unsub()
   }, []);
 
   return (
