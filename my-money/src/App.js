2cd467675d18ee99c08cbffe68bdf23f0ad39967
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-07 05:57:15 -0500

    CHALLENGE - Creating Pages & Routes

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+
+//pages
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
