ffae54879563a7554887c02bf5115800244763c6
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-08 18:24:39 -0500

    Firebase Init

--- the-dojo/src/firebase/config.js
+++ the-dojo/src/firebase/config.js
@@ -3,8 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-
-}
+    apiKey: "AIzaSyBxbIeSkJBGpw5xjbPvO15N4b54sTOvuFw",
+    authDomain: "thedojosite-9c5a5.firebaseapp.com",
+    projectId: "thedojosite-9c5a5",
+    storageBucket: "thedojosite-9c5a5.appspot.com",
+    messagingSenderId: "61027733968",
+    appId: "1:61027733968:web:fe3e5dd8a2c2edd7bda9d5"
+  };
 
 // init firebase
 firebase.initializeApp(firebaseConfig)
