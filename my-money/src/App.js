e28fb176022f6c2a5f758f9320d7f1db93c7d9c2
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-08 05:28:24 -0500

    Route Guarding

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,27 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useAuthContext } from './hooks/useAuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 //pages
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup';
-import Navbar from './components/Navbar';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const { authIsReady } = useAuthContext()
-
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className="App">
-      { authIsReady && (
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Signup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </BrowserRouter>
       )}
     </div>
   );
