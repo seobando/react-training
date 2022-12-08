f119a06714436ad4e56810efe466b7274c5a283c
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-08 05:41:04 -0500

    Making a Transaction Form

--- my-money/src/pages/home/Home.js
+++ my-money/src/pages/home/Home.js
@@ -1,9 +1,15 @@
 import styles from './Home.module.css'
+import TransactionForm from './TransactionForm'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div clasName={styles.sidebar}>
+        <TransactionForm/>
+      </div>
     </div>
   )
 }
