5004a18b153c350094c8b3044b537791ad8b7951
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-08 07:11:49 -0500

    Deleting Transactions

--- my-money/src/hooks/useFirestore.js
+++ my-money/src/hooks/useFirestore.js
@@ -19,6 +19,13 @@ const firestoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case "DELETED_DOCUMENT":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     case "ERROR":
       return {
         isPending: false,
@@ -64,7 +71,21 @@ export const useFirestore = (collection) => {
   };
 
   // delete a document
-  const deleteDocument = async (id) => {};
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({
+        type: "DELETE_DOCUMENT",
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({
+        type: "ERROR",
+        payload: "could not delete.",
+      });
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
