16cb456cde0c7b37b2734f060fbba2a4fd4d7dde
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 06:43:47 -0500

    Section 17 project b UI ld   project management site (#12)
    * Project Preview and Setup
    
    * Firebase Setup
    
    * Reusing Firebase Hooks and Auth Context
    
    * Reusing Firebase Hooks and Auth Context
    
    * Firebase Init
    
    * Router & Pages Setup
    
    * Navbar Component
    
    * Sidebar Component
    
    * Creating a Signup Page
    
    * Handling File Inputs
    
    * Firebase Storage Setup
    
    * Uploading Profile Images
    
    * Signing a User Up
    
    * Creating User Documents
    
    * Signin and Signout
    
    * Redirect and Route Guards
    
    * CHALLENGE Conditional Navbar Links
    
    * Use Avatar Component
    
    * Fetching Users
    
    * Showing Users Online
    
    * Making the "Creat Project"
    
    * Using React-Select
    
    * Assigning Users
    
    * Setting Form Errors
    
    * Creating a Project Object
    
    * CHALLENGE - Saving New Projects
    
    * Fetching Projects
    
    * Making a Project List / Grid
    
    * Making a useDocument Hook
    
    * Project Details Page
    
    * Project Summary Component
    
    * Making a Comments Form
    
    * Updating Firestore
    
    * Adding Comments
    
    * Listing Comments
    
    * Completing / Deleting Projects
    
    * Making a Filter (part 1)
    
    * Making a Filter (part 2)
    
    * Making a Filter
    
    * Adding Firestore Rules
    
    * Final Touches
    
    * Deploy

---
+++ the-dojo/src/App.js
@@ -0,0 +1,58 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+// styles
+import "./App.css";
+
+// pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import OnlineUsers from "./components/OnlineUsers";
+
+function App() {
+  const { authIsReady, user } = useAuthContext();
+
+  return (
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/project:id"
+                element={user ? <Project /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              />
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              />
+            </Routes>
+          </div>
+          {user && <OnlineUsers />}
+        </BrowserRouter>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ the-dojo/src/firebase/config.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxbIeSkJBGpw5xjbPvO15N4b54sTOvuFw",
+    authDomain: "thedojosite-9c5a5.firebaseapp.com",
+    projectId: "thedojosite-9c5a5",
+    storageBucket: "thedojosite-9c5a5.appspot.com",
+    messagingSenderId: "61027733968",
+    appId: "1:61027733968:web:fe3e5dd8a2c2edd7bda9d5"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore
+
+
+export { projectFirestore, projectAuth, projectStorage, timestamp }
