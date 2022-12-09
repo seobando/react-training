97e04578f8afc9e69f32e1b0cac95bbfe6a80070
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-08 19:13:33 -0500

    Sidebar Component

--- the-dojo/src/App.js
+++ the-dojo/src/App.js
@@ -9,14 +9,16 @@ import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
-          <Navbar/>
+          <Navbar />
           <Routes>
             <Route exact path="/" element={<Dashboard />} />
             <Route path="/create" element={<Create />} />
