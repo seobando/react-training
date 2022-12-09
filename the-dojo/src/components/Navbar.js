0cab080365ff4320dccb290d1e652e1b4a631b29
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 05:17:15 -0500

    Signin and Signout

--- the-dojo/src/components/Navbar.js
+++ the-dojo/src/components/Navbar.js
@@ -1,27 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 // styles & images
-import "./Navbar.css";
-import Temple from "../assets/temple.svg";
+import './Navbar.css'
+import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
+  const { logout, isPending } = useLogout()
+
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <ul>
         <li className="logo">
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
+
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
         <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn">Logout</button>
+          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+          {isPending && <button className="btn" disabled>Logging out...</button>}
         </li>
       </ul>
-    </div>
-  );
+    </nav>
+  )
 }
