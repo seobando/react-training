f99b5912427dd2d02d5917dadc73952a148b5af4
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 06:24:29 -0500

    Making a Filter (part 2)

--- the-dojo/src/pages/dashboard/ProjectFilter.js
+++ the-dojo/src/pages/dashboard/ProjectFilter.js
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+const filterList = [
+  "all",
+  "mine",
+  "development",
+  "design",
+  "marketing",
+  "sales",
+];
 
-export default function ProjectFilter() {
-  const [currentFilter, setCurrentFilter] = useState('all')
+export default function ProjectFilter({ changeFilter }) {
+  const [currentFilter, setCurrentFilter] = useState("all");
 
   const handleClick = (newFilter) => {
-    setCurrentFilter(newFilter)
-  }
-  console.log(currentFilter)
+    setCurrentFilter(newFilter);
+    changeFilter(newFilter);
+  };
 
   return (
     <div className="project-filter">
       <nav>
         <p>Filter by: </p>
         {filterList.map((f) => (
-          <button key={f}
+          <button
+            key={f}
             onClick={() => handleClick(f)}
-            className={currentFilter === f ? 'active' : ''}
-          >{f}</button>
+            className={currentFilter === f ? "active" : ""}
+          >
+            {f}
+          </button>
         ))}
       </nav>
     </div>
-  )
+  );
 }
