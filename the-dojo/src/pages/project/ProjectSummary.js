e5207d7454c746d490c92637730b9549efcb0b5a
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 06:19:12 -0500

    Completing / Deleting Projects

--- the-dojo/src/pages/project/ProjectSummary.js
+++ the-dojo/src/pages/project/ProjectSummary.js
@@ -1,6 +1,18 @@
 import Avatar from "../../components/Avatar";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    deleteDocument(project.id);
+    navigate("/");
+  };
+
   return (
     <div>
       <div className="project-summary">
@@ -18,6 +30,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
