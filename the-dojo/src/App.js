0404a73d063190dfb4cd95b2bb6c69483468abcb
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 05:25:06 -0500

    Redirect and Route Guards

--- the-dojo/src/App.js
+++ the-dojo/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // styles
 import "./App.css";
@@ -13,21 +14,41 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Dashboard />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/project:id" element={<Project />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/project:id"
+                element={user ? <Project /> : <Navigate to="Login" />}
+              />
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              />
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
