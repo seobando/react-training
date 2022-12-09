82d49720009300cda7d08b9af3a3f9474bed7efa
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 06:22:31 -0500

    Making a Filter (part 1)

--- the-dojo/src/pages/dashboard/Dashboard.js
+++ the-dojo/src/pages/dashboard/Dashboard.js
@@ -1,19 +1,21 @@
-import { useCollection } from '../../hooks/useCollection'
+import { useCollection } from "../../hooks/useCollection";
 
 // components
-import ProjectList from '../../components/ProjectList'
+import ProjectList from "../../components/ProjectList";
 
 // styles
-import './Dashboard.css'
+import "./Dashboard.css";
+import ProjectFilter from "./ProjectFilter";
 
 export default function Dashboard() {
-  const { documents, error } = useCollection('projects')
+  const { documents, error } = useCollection("projects");
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
-  )
+  );
 }
