e214759ee5fd1665d093bc6b24a84da2b580bac1
Sebastian OM <sebastianobandomorales@gmail.com>
2022-12-09 05:39:05 -0500

    Showing Users Online

--- the-dojo/src/components/OnlineUsers.js
+++ the-dojo/src/components/OnlineUsers.js
@@ -1,26 +1,26 @@
-import { useCollection } from "../hooks/useCollection";
+import { useCollection } from '../hooks/useCollection'
 
 // components
-import Avatar from "./Avatar";
+import Avatar from './Avatar'
 
 // styles
-import "./OnlineUsers.css";
+import './OnlineUsers.css'
 
 export default function OnlineUsers() {
-  const { isPending, error, documents } = useCollection("users");
+  const { isPending, error, documents } = useCollection('users')
 
   return (
     <div className="user-list">
       <h2>All Users</h2>
       {isPending && <div>Loading users...</div>}
       {error && <div>{error}</div>}
-      {documents &&
-        documents.map((user) => (
-          <div key={user.id} className="user-list-item">
-            <span>{user.displayName}</span>
-            <Avatar src={user.photoURL} />
-          </div>
-        ))}
+      {documents && documents.map(user => (
+        <div key={user.id} className="user-list-item">
+          {user.online && <span className="online-user"></span>}
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />
+        </div>
+      ))}
     </div>
-  );
+  )
 }
